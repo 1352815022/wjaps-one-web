8d5028bca972d61c4e2dc428ca2015c82226561a
1382515022@qq.com <1382515022@qq.com>
2023-02-22 09:16:47 +0800

    调整高度

--- src/pages/Order/PlanInner/index.jsx
+++ src/pages/Order/PlanInner/index.jsx
@@ -886,7 +886,7 @@ class PlanInner extends Component {
 
     return (
       <PageWrapper className={cls(styles['container-box'])}>
-        <ExtTable onTableRef={inst => (this.tableRef = inst)} {...this.getExtableProps()} />
+        <ExtTable onTableRef={inst => (this.tableRef = inst)} {...this.getExtableProps()} style={{height:"600px"}}/>
         {modalVisible ? <EditModal {...this.getEditModalProps()} /> : null}
         {orderPlanModalVisible ? <OrderPlanModal {...this.getOrderPlanModalProps()} /> : null}
         {batchOrderModalVisible ? <BatchOrderModal {...this.getBatchOrderModalProps()} /> : null}
